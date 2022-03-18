5b3695fb038bf53fc15e82218401f5961ec881b7
GitHub Action <action@github.com>
2022-03-18 14:54:14 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/suikelei/precommithookplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.suikelei.precommithookplugin.services
+
+import com.github.suikelei.precommithookplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
